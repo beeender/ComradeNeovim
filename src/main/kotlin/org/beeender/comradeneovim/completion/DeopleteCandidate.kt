6f4ba6a417c3e7e76b49fd8b712e4170754dd9c2
Chen Mulong <chenmulong@gmail.com>
2019-01-30 13:41:55 +0800

    Filter out snippets in candidates

--- src/main/kotlin/org/beeender/comradeneovim/completion/DeopleteCandidate.kt
+++ src/main/kotlin/org/beeender/comradeneovim/completion/DeopleteCandidate.kt
@@ -1,13 +1,37 @@
 package org.beeender.comradeneovim.completion
 
+import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.codeInsight.lookup.LookupElementPresentation
 
-class Candidate : LookupElementPresentation() {
-    fun toMap() : Map<String, String?>
+class Candidate(lookupElement: LookupElement) : LookupElementPresentation() {
+    init {
+        lookupElement.renderElement(this)
+    }
+
+    // This should be added as candidate
+    val valuable : Boolean get() {
+        // When isTypedGrayed is true, for at least IDEA, it is the snippet completion.
+        return itemText != null && itemText!!.isNotBlank() && !isTypeGrayed
+    }
+
+    override fun isReal(): Boolean {
+        return false
+    }
+
+    private fun toMap() : Map<String, String>
     {
         return mapOf(
-                "word" to itemText,
-                "abbr" to itemText + tailText,
-                "kind" to typeText)
+                "word" to (itemText ?: ""),
+                "abbr" to (itemText ?: "") + (tailText ?: ""),
+                "kind" to (typeText ?: ""))
+    }
+
+    companion object {
+        fun addCandidate(list: MutableList<Map<String, String>>, lookupElement: LookupElement) {
+            val can = Candidate(lookupElement)
+            if (can.valuable) {
+                list.add(can.toMap())
+            }
+        }
     }
 }
