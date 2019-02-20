118a7c861787c7a62d08487dd6333ecf0e8c7b2a
Chen Mulong <chenmulong@gmail.com>
2019-02-20 21:57:52 +0800

    Add detach api

--- src/main/kotlin/org/beeender/neovim/BufferApi.kt
+++ src/main/kotlin/org/beeender/neovim/BufferApi.kt
@@ -3,10 +3,15 @@ package org.beeender.neovim
 import org.beeender.neovim.rpc.Notification
 import org.msgpack.core.MessagePack
 import org.msgpack.jackson.dataformat.MessagePackExtensionType
+import java.lang.IllegalArgumentException
 
 class BufferApi internal constructor(private val client: Client) {
     fun attach(id: Int, sendBuf: Boolean) {
         client.requestOnly("nvim_buf_attach", listOf(id, sendBuf, emptyMap<Any?, Any?>()))
+    }
+
+    fun detach(id: Int) {
+        client.requestOnly("nvim_buf_detach", listOf(id))
     }
 
     suspend fun getName(id: Int) : String {
@@ -28,8 +33,14 @@ class BufferApi internal constructor(private val client: Client) {
     }
 
     companion object {
-        fun decodeBufId(notification: Notification) : Int {
-            return MessagePack.newDefaultUnpacker((notification.args[0] as MessagePackExtensionType).data).unpackInt()
+        fun decodeBufId(obj: Any?) : Int {
+            if (obj is Notification) {
+                return MessagePack.newDefaultUnpacker((obj.args[0] as MessagePackExtensionType).data).unpackInt()
+            }
+            else if (obj is MessagePackExtensionType) {
+                return MessagePack.newDefaultUnpacker(obj.data).unpackInt()
+            }
+            throw IllegalArgumentException("The given object cannot be decoded as a Buffer ID. \n $obj")
         }
     }
 }
