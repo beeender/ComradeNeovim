41425099dcc32d245436cc799242d122e28112ad
Chen Mulong <chenmulong@gmail.com>
2019-03-16 00:20:08 +0800

    Rename

--- src/main/kotlin/org/beeender/comradeneovim/core/Constants.kt
+++ src/main/kotlin/org/beeender/comradeneovim/core/Constants.kt
@@ -1,5 +1,5 @@
 package org.beeender.comradeneovim.core
 
-const val FUN_JETBRAIN_REGISTER = "comrade#jetbrain#register"
+const val FUN_JETBRAIN_REGISTER = "comrade#jetbrain#Register"
 const val FUN_BUFFER_REGISTER = "comrade#buffer#Register"
 const val FUN_SET_INSIGHT = "comrade#SetInsights"
