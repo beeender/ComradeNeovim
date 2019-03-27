006545129e6e1a31da94f4ceba958f7e935d6e01
Chen Mulong <chenmulong@gmail.com>
2019-03-27 10:00:36 +0800

    Delegate implements Editor instead of EditorEx
    to avoid 'Abstract method is not implemented' on version 191.

--- src/main/kotlin/org/beeender/comradeneovim/buffer/EditorDelegate.kt
+++ src/main/kotlin/org/beeender/comradeneovim/buffer/EditorDelegate.kt
@@ -1,6 +1,7 @@
 package org.beeender.comradeneovim.buffer
 
 import com.intellij.openapi.editor.CaretModel
+import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.ex.EditorEx
 import com.intellij.openapi.externalSystem.service.execution.NotSupportedException
 import javax.swing.JComponent
@@ -14,7 +15,7 @@ import javax.swing.JComponent
 class NotSupportedByUIDelegateException(val editor: EditorEx, val methodName: String) :
         NotSupportedException("$methodName is not supported by EditorDelegate for ${editor.virtualFile.name}")
 
-class EditorDelegate(val editor: EditorEx) : EditorEx by editor {
+class EditorDelegate(val editor: EditorEx) : Editor by editor {
 
     override fun getComponent(): JComponent {
         throw NotSupportedByUIDelegateException(editor, "getComponent")
