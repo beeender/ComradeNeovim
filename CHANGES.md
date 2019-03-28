8e5a615e9d820c26f5be82973d631d940adbe3c9
Chen Mulong <chenmulong@gmail.com>
2019-03-28 10:57:08 +0800

    Release 0.1.1

--- CHANGES.md
+++ CHANGES.md
@@ -1,6 +1,9 @@
 Changelog
 =========
 
+0.1.1 - 20190328
+- Fix `Abstract method is not implemented` for 2019.1.
+
 0.1.0 - 20190326
 - First release.
 - Basic support for code completion.
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "org.beeender"
-version = "0.1.1-SNAPSHOT"
+version = "0.1.1"
 
 buildscript {
     repositories { mavenCentral() }
