5a98fd87d892c2e89e592d8f0499725fe25b1cf5
JohnGrib <johngrib82@gmail.com>
2024-09-08 11:11:59 +0900

    Try Version Up

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,48 +1,51 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-group = "org.beeender"
-version = "0.1.3-SNAPSHOT"
-
-buildscript {
-    repositories { mavenCentral() }
-    dependencies {
-        classpath(kotlin("gradle-plugin", "1.3.21"))
-    }
-}
-
 plugins {
-    id("org.jetbrains.intellij") version "0.4.7"
-    kotlin("jvm") version "1.3.21"
+    id("org.jetbrains.intellij") version "1.13.3"
+    kotlin("jvm") version "1.8.20"
 }
 
-repositories { mavenCentral() }
+group = "org.beeender"
+version = "0.1.5-SNAPSHOT"
+
+repositories { 
+    mavenCentral()
+    maven { url = uri("https://www.jetbrains.com/intellij-repository/releases") }
+    maven { url = uri("https://cache-redirector.jetbrains.com/intellij-dependencies") }
+}
+
 dependencies {
-    compile("org.msgpack", "msgpack-core", "0.8.16")
-    compile("org.msgpack", "jackson-dataformat-msgpack", "0.8.16")
-    compile("com.fasterxml.jackson.module", "jackson-module-kotlin", "2.9.8")
-    compile("org.scala-sbt.ipcsocket", "ipcsocket", "1.0.0")
+    implementation("org.msgpack:msgpack-core:0.9.5")
+    implementation("org.msgpack:jackson-dataformat-msgpack:0.9.5")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.0")
+    implementation("org.scala-sbt.ipcsocket:ipcsocket:1.6.1")
     implementation(kotlin("stdlib"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.1.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.0")
     implementation("com.kohlschutter.junixsocket:junixsocket-core:2.6.2")
-    testCompile("junit","junit", "4.12")
-    testImplementation("io.mockk", "mockk", "1.9")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("io.mockk:mockk:1.13.5")
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        jvmTarget = "1.8"
+intellij {
+    version.set("2023.1")
+    type.set("IC") // IntelliJ IDEA Community Edition
+    updateSinceUntilBuild.set(false)
+    instrumentCode.set(false)
+}
+
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "17"
+        }
+    }
+
+    buildSearchableOptions {
+        enabled = false
     }
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-}
-
-intellij {
-    updateSinceUntilBuild = false
-    // If the instrumentCode is enabled, the Test cannot call internal method.
-    // See https://github.com/JetBrains/gradle-intellij-plugin/issues/230
-    instrumentCode = false
-    version = "2019.1"
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
