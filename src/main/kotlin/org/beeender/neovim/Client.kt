021d8f28d8731636397d8aa84450a3f5da639884
JohnGrib <johngrib82@gmail.com>
2024-09-08 11:55:49 +0900

    Replace Deprecated offer to trySend

--- src/main/kotlin/org/beeender/neovim/Client.kt
+++ src/main/kotlin/org/beeender/neovim/Client.kt
@@ -55,7 +55,7 @@ class Client(connection: NeovimConnection, onClose: (Throwable?) -> Unit) {
 
     init {
         receiver.start({msg ->
-            ReceiverChannel.offer(this to msg)
+            ReceiverChannel.trySend(this to msg)
         }, onClose)
     }
 
@@ -65,10 +65,10 @@ class Client(connection: NeovimConnection, onClose: (Throwable?) -> Unit) {
         val channel = Channel<Response>()
         val handler: (Response) -> Unit = { rsp: Response ->
             ret.set(rsp)
-            channel.offer(rsp)
+            channel.trySend(rsp)
         }
         resHandlers[req.id] = handler
-        SenderChannel.offer(this to req)
+        SenderChannel.trySend(this to req)
         return channel.receive()
     }
 
@@ -130,7 +130,7 @@ class Client(connection: NeovimConnection, onClose: (Throwable?) -> Unit) {
                         Response(msg, t.toString(), null)
                     }
             }
-            SenderChannel.offer(this to rsp)
+            SenderChannel.trySend(this to rsp)
         }
         else if (msg is Notification) {
             if (notiHandlers.containsKey(msg.name)) {
